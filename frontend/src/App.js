918da019e2967257865c64d1d3132ec8b4dec2fe
Christopher Arnold <chrisarnold2010@gmail.com>
2018-10-31 17:43:04 -0500

    added frontend auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,71 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    email: "",
+    password: "",
+    currentUser: {}
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/profile", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${localStorage.token}`
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (!data.error) {
+          this.setState({
+            currentUser: data
+          })
+        }
+    })
+      // console.log(localStorage.token)
+  }
+
+  logIn = event => {
+    event.preventDefault();
+
+    fetch("http://localhost:3000/login", {
+      method: "POST",
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(resp => resp.json())
+      .then(data => {
+        if (!data.error) {
+          localStorage.token = data.token;
+          this.setState({
+            currentUser: data.user
+          });
+        } else {
+          this.setState({
+            loginError: data.error
+          });
+        }
+      });
+  };
+
+  logOut = () => {
+    // event.preventDefault();
+    // debugger
+    this.setState({
+      email: "",
+      password: "",
+      currentUser: {}
+    })
+
+    localStorage.token = "";
+  }
+
   render() {
     return (
       <div className="App">
