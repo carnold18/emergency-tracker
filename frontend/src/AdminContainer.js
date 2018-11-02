1d8bb628217b140aa7a7cabbe1f07673c1df5560
Christopher Arnold <chrisarnold2010@gmail.com>
2018-11-02 16:56:42 -0500

    added zipcodselector component, not functional yet

--- frontend/src/AdminContainer.js
+++ frontend/src/AdminContainer.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import Map from './Map';
+import ZipCodeSelector from './ZipCodeSelector';
 
 class AdminContainer extends Component {
     render() {
         return (
-            <Map googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
-            loadingElement={<div style={{ height: `100%` }} />}
-            containerElement={<div style={{ height: `400px` }} />}
-            mapElement={<div style={{ height: `100%` }} />}
-            />
+            <div>
+                <Map googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: `400px` }} />}
+                mapElement={<div style={{ height: `100%` }} />}
+                />
+                <ZipCodeSelector />
+            </div>
         )
     }
 }
---
+++ frontend/src/ZipCodeSelector.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ZipCodeSelector extends Component {
+
+    render() {
+        return (
+        <div>
+            ZipCodeSelector
+        </div>
+        )
+    }
+}
+
+export default ZipCodeSelector;
