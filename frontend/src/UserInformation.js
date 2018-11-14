8f8a9a6e4306bcd338bf3fa979350403cbde9315
Christopher Arnold <chrisarnold2010@gmail.com>
2018-11-14 10:54:08 -0600

    added color circles to user page based on user status

--- frontend/src/UserInformation.js
+++ frontend/src/UserInformation.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import EditUserForm from './EditUserForm';
+import UserTypeButton from './UserTypeButton';
+import UserStatusButton from './UserStatusButton';
 
 class UserInformation extends Component {
 
@@ -17,19 +19,23 @@ class UserInformation extends Component {
     render() {
         return(
             <div className="userinformation">
-                {/* <h2>Welcome, {this.props.currentUser.first_name}</h2> */}
-                <h3>Your current status is: {this.props.currentUser.status}</h3>
                 <div>
-                    <p>{this.props.currentUser.address_line_1}</p>
-                    <p>{this.props.currentUser.address_line_2}</p>
-                    <p>{this.props.currentUser.city}, {this.props.currentUser.state} {this.props.currentUser.zip_code}</p>
-                    <p>{this.props.currentUser.phone_number}</p>
+                    {/* <h2>Welcome, {this.props.currentUser.first_name}</h2> */}
+                    <div>
+                        <p>{this.props.currentUser.address_line_1}</p>
+                        <p>{this.props.currentUser.address_line_2}</p>
+                        <p>{this.props.currentUser.city}, {this.props.currentUser.state} {this.props.currentUser.zip_code}</p>
+                        <p>{this.props.currentUser.phone_number}</p>
+                    </div>
+                    <button onClick={this.changeEditState}>Edit?</button>
+                    { this.state.editUser ? 
+                        ( <EditUserForm currentUser={this.props.currentUser} /> )
+                        : null
+                    }
                 </div>
-                <button onClick={this.changeEditState}>Edit?</button>
-                { this.state.editUser ? 
-                    ( <EditUserForm currentUser={this.props.currentUser} /> )
-                    : null
-                }
+                <div>
+                    <UserStatusButton currentUser={this.props.currentUser} />
+                </div>
             </div>
         )
     }
--- frontend/src/UserStatusButton.js
+++ frontend/src/UserStatusButton.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import red from './red-circle-64.png';
+import green from './green-circle-64.png';
+import yellow from './yellow-circle-64.png';
 
 class UserStatusButton extends Component {
-
 
     render() {
         return(
             <div>
-                {}
+            {this.props.currentUser.status === 0 ? (
+                <img src={green} alt="no emergency" />
+            ) : null }
+            {this.props.currentUser.status === 1 ? (
+                <img src={yellow} alt="mid emergency" />
+            ) : null }
+            {this.props.currentUser.status === 2 ? (
+                <img src={red} alt="high emergency" />
+            ) : null }
             </div>
         )
     }
