3d3ab741c054703ae730600772d2d5452cd92727
Christopher Arnold <chrisarnold2010@gmail.com>
2018-11-14 22:53:43 -0600

    more styling

--- frontend/src/StatsChart.js
+++ frontend/src/StatsChart.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Chart } from 'react-google-charts';
 
 class StatsChart extends Component {
 
@@ -8,12 +9,26 @@ class StatsChart extends Component {
     
     render() {
         return(
-            <div>
-                <h3>Information on </h3>
+            // <div>
+            //     <Chart 
+            //         chartType="Histogram"
+            //         width={'500px'}
+            //         height={'350px'}
+            //         loader={<div>Loading Chart</div>}
+            //         data={[
+            //             ["User Status", "Value"],
+            //             ["OK", "green"],
+            //             ["Distress", "yellow"],
+            //             ["Danger", "red"]
+            //         ]}
+            //     />
+            // </div>
+            <div id="stats-chart">
+                <br /><h3>Selected Zone Percentages</h3>
                 <table>
-                    <tr>
-                        <td>Area Percentages</td>
-                    </tr>
+                    {/* <tr>
+                        <td>Selected Zone Percentages</td>
+                    </tr> */}
                     <tr>
                         <td>{Math.round(this.props.zeroPerc)}% OK</td>
                     </tr>
